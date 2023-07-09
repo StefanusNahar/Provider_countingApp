edc31131a0487c36c4bdfa8a0becbcec46f08f6d
StefanusNahar <129366124+StefanusNahar@users.noreply.github.com>
2023-07-09 21:13:23 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Counting App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Counting App'),
